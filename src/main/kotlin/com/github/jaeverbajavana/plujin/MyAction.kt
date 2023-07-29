80f2dfb494cbac7dbade09b88960b4a3b6003d77
GitHub Action <action@github.com>
2023-07-29 10:54:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/jaeverbajavana/plujin/MyAction.kt
+++ src/main/kotlin/com/github/jaeverbajavana/plujin/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jaeverbajavana.plujin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/jaeverbajavana/plujin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jaeverbajavana/plujin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jaeverbajavana.plujin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
